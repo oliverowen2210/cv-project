1001eb3d85a1a70cd883b581b4249888ebf5c9c8
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-29 09:13:09 -0500

    Added placeholder "TextField" component

---
+++ src/components/TextField.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class InputField extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
+}
