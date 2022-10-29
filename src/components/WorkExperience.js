c46122f2b09420f70c1712b2d4f3a897d4b84055
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-29 09:12:38 -0500

    Added placeholder "WorkExperience" component

---
+++ src/components/WorkExperience.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class WorkExperience extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <></>;
+  }
+}
