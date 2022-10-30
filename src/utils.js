1b4ecbfdb726031c0fa1a712dea7fce05af8ba65
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-29 22:39:48 -0500

    Added two importable functions for info components

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+export function updateInputs() {
+  let inputs = { ...this.state.inputs };
+  Object.values(inputs).forEach((input) => {
+    input.value = input.inputValue;
+  });
+  this.setState({ inputs: inputs });
+}
+
+export function updateValue(e, id) {
+  let inputs = { ...this.state.inputs };
+  Object.values(inputs).some((input) => {
+    if (input.id === id) {
+      input.inputValue = e.target.value;
+      this.setState({ inputs: inputs });
+      return true;
+    }
+    return false;
+  });
+}
