6030f1dabbaeae070fe9c902f8f8c22303b2f0f9
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-30 11:06:20 -0600

    moved input data to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import GeneralInfo from "./components/GeneralInfo";
 import EducationInfo from "./components/EducationInfo";
+import uniqid from "uniqid";
 
 function App() {
+  const inputs = {
+    generalInputs: {
+      name: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+        valid: true,
+        required: true,
+      },
+      address: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+        valid: true,
+        required: true,
+      },
+      email: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+        valid: true,
+        required: true,
+      },
+      phone: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+        valid: true,
+        required: true,
+      },
+    },
+    educationInputs: {
+      degree: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+      },
+      school: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+      },
+      minors: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+      },
+      gradDate: {
+        value: "",
+        inputValue: "",
+        id: uniqid(),
+      },
+    },
+  };
   return (
     <div className="App">
       <GeneralInfo></GeneralInfo>
