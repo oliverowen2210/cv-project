9707400ac8216608bb60f3d57e40b3c89506cc03
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-29 22:40:33 -0500

    updated edited input submission

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,44 +1,128 @@
 import React from "react";
 import uniqid from "uniqid";
 
+import { updateInputs, updateValue } from "../utils";
+import TextField from "./TextField";
+
 export default class GeneralInfo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      editing: true,
       inputs: {
         name: {
           value: "",
-          editing: false,
+          inputValue: "",
           id: uniqid(),
         },
         address: {
           value: "",
-          editing: false,
+          inputValue: "",
           id: uniqid(),
         },
         email: {
           value: "",
-          editing: false,
+          inputValue: "",
           id: uniqid(),
         },
         phone: {
           value: "",
-          editing: false,
+          inputValue: "",
           id: uniqid(),
         },
       },
     };
+    this.updateInputs = updateInputs.bind(this);
+    this.updateValue = updateValue.bind(this);
   }
+
+  submitButtonHandling = (e) => {
+    e.preventDefault();
+    this.updateInputs();
+    this.setState({ editing: false });
+  };
+
+  editButtonHandling = (e) => {
+    e.preventDefault();
+    this.setState({ editing: true });
+  };
+
   render() {
+    const { name, address, email, phone } = this.state.inputs;
     return (
-      <div>
-        <h1 className="name">{this.state.inputs.name.value}Placeholder Name</h1>
-        <div>
-          <span className="address">🏠: {this.state.inputs.address.value}</span>
-          <span className="email">📧: {this.state.inputs.email.value}</span>
-          <span className="phone">📞: {this.state.inputs.phone.value}</span>
+      <form className="generalInfo">
+        <div className="inputGroup nameInput">
+          {this.state.editing ? <label htmlFor={name.id}>Name: </label> : <></>}
+          <TextField
+            class="name"
+            value={name.inputValue}
+            id={name.id}
+            beingEdited={this.state.editing}
+            updateFunc={this.updateValue}
+          ></TextField>
         </div>
-      </div>
+        <div className="inputGroup addressInput">
+          {this.state.editing ? (
+            <label htmlFor={address.id}>Address</label>
+          ) : (
+            <span>🏠</span>
+          )}
+          <TextField
+            class="address"
+            value={address.inputValue}
+            id={address.id}
+            beingEdited={this.state.editing}
+            updateFunc={this.updateValue}
+          ></TextField>
+        </div>
+        <div className="inputGroup emailInput">
+          {this.state.editing ? (
+            <label htmlFor={email.id}>Email</label>
+          ) : (
+            <span>📧</span>
+          )}
+          <TextField
+            class="email"
+            value={email.inputValue}
+            id={email.id}
+            beingEdited={this.state.editing}
+            updateFunc={this.updateValue}
+          ></TextField>
+        </div>
+        <div className="inputGroup phoneInput">
+          {this.state.editing ? (
+            <label htmlFor={phone.id}>Phone Number</label>
+          ) : (
+            <span>📞</span>
+          )}
+          <TextField
+            class="phone"
+            value={phone.inputValue}
+            id={phone.id}
+            beingEdited={this.state.editing}
+            updateFunc={this.updateValue}
+          ></TextField>
+        </div>
+        {this.state.editing ? (
+          <button
+            className="submitButton"
+            onClick={(e) => {
+              this.submitButtonHandling(e);
+            }}
+          >
+            ✓
+          </button>
+        ) : (
+          <button
+            className="editButton"
+            onClick={(e) => {
+              this.editButtonHandling(e);
+            }}
+          >
+            ✎
+          </button>
+        )}
+      </form>
     );
   }
 }
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -5,7 +5,24 @@ export default class InputField extends React.Component {
     super(props);
     this.state = {};
   }
+
   render() {
-    return <></>;
+    let returnedElement = null;
+    if (this.props.beingEdited)
+      returnedElement = (
+        <input
+          id={this.props.id}
+          className={this.props.class}
+          onChange={(e) => {
+            this.props.updateFunc(e, this.props.id);
+          }}
+          value={this.props.value}
+        />
+      );
+    else
+      returnedElement = (
+        <span className={this.props.class}>{this.props.value}</span>
+      );
+    return returnedElement;
   }
 }
