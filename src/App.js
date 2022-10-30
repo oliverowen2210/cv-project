a6711d726062ba7255bca63976fe10c11863f29e
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-30 10:00:57 -0600

    Added EducationInfo component to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import GeneralInfo from "./components/GeneralInfo";
+import EducationInfo from "./components/EducationInfo";
 
 function App() {
   return (
     <div className="App">
       <GeneralInfo></GeneralInfo>
+      <EducationInfo></EducationInfo>
     </div>
   );
 }
