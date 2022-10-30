ef7cdcdb0ea4af90e476a175461902c2fb5e5a5c
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-30 11:08:40 -0600

    moved utils functions to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,38 @@ import EducationInfo from "./components/EducationInfo";
 import uniqid from "uniqid";
 
 function App() {
+  function updateInputs() {
+    let inputs = { ...this.state.inputs };
+    Object.values(inputs).forEach((input) => {
+      input.value = input.inputValue;
+    });
+    this.setState({ inputs: inputs });
+  }
+
+  function updateValue(e, id) {
+    let inputs = { ...this.state.inputs };
+    Object.values(inputs).some((input) => {
+      if (input.id === id) {
+        input.inputValue = e.target.value;
+        this.setState({ inputs: inputs });
+        return true;
+      }
+      return false;
+    });
+  }
+
+  function toggleInputValidity(id, newState) {
+    let inputs = { ...this.state.inputs };
+    Object.values(inputs).some((input) => {
+      if (input.id === id) {
+        input.valid = newState;
+        this.setState({ inputs: inputs });
+        return true;
+      }
+      return false;
+    });
+  }
+
   const inputs = {
     generalInputs: {
       name: {
