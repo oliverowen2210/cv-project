d0f93a4e3cdfdf19f170898fd9e7b823fb44af59
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-30 18:36:32 -0600

    added validation to Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -18,25 +18,65 @@ export default class Info extends React.Component {
         key={`${input.id} wrapper`}
       >
         <EditableLabel
-          editing={this.state.editing}
+          //* this is because useState is async, so checking for
+          /* validity status after submitting doesn't work. will revisit once
+        i learn how to use hooks*/
+          beingEdited={this.props.allInputsValid ? this.state.editing : true}
           htmlFor={input.id}
           edit={input.edit}
           noEdit={input.noEdit}
           key={`${input.id} label`}
+          required={input.required}
         />
         <TextField
-          class={input.name}
-          value={input.inputValue}
-          id={input.id}
-          beingEdited={this.state.editing}
-          loop={this.props.loop}
+          input={input}
+          beingEdited={this.props.allInputsValid ? this.state.editing : true}
+          update={this.props.update}
+          validate={this.validate}
           key={`${input.id} field`}
         ></TextField>
       </div>
     ));
   }
 
-  updateInputValues() {
+  validateRequired(input, inputValue) {
+    if (input.required) {
+      if (!inputValue) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  validateLength(input, inputValue) {
+    if (input.minLength && inputValue.length < input.minLength) {
+      return false;
+    } else if (input.maxLength && inputValue.length > input.maxLength) {
+      return false;
+    }
+    return true;
+  }
+
+  validatePattern(input, inputValue) {
+    if (input.pattern) {
+      const pattern = new RegExp(input.pattern);
+      return pattern.test(inputValue);
+    }
+    return true;
+  }
+
+  validate = (input, inputValue) => {
+    if (
+      !this.validateLength(input, inputValue) ||
+      !this.validatePattern(input, inputValue) ||
+      !this.validateRequired(input, inputValue)
+    ) {
+      return this.props.toggle(input.id, false);
+    }
+    return this.props.toggle(input.id, true);
+  };
+
+  updateValues() {
     this.props.loop((input) => {
       input.value = input.inputValue;
     });
@@ -44,8 +84,16 @@ export default class Info extends React.Component {
 
   submitButtonHandling = (e) => {
     e.preventDefault();
-    this.updateInputValues();
+    this.props.inputs.forEach((input) => {
+      this.validate(input, input.inputValue);
+    });
+    if (!this.props.allInputsValid)
+      throw new Error("One or more input fields are invalid!");
+    this.updateValues();
     this.setState({ editing: false });
+    setTimeout(() => {
+      if (!this.props.allInputsValid) this.setState({ editing: true });
+    }, 1000);
   };
 
   editButtonHandling = (e) => {
@@ -55,11 +103,11 @@ export default class Info extends React.Component {
 
   render() {
     return (
-      <div className="generalInfo">
+      <div className={this.props.class}>
         {this.getInputHTML()}
 
         <EditButton
-          editing={this.state.editing}
+          beingEdited={this.props.allInputsValid ? this.state.editing : true}
           editFunc={this.editButtonHandling}
           submitFunc={this.submitButtonHandling}
         />
