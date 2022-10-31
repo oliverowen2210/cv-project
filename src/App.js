9f7cbb665e59710f7033c6626628be730fac81a7
oliverowen2210 <oliverowen2210@gmail.com>
2022-10-30 18:35:52 -0600

    added new input keys and functions to get/set them

--- src/App.js
+++ src/App.js
@@ -13,9 +13,10 @@ function App() {
         value: "",
         inputValue: "",
         id: uniqid(),
-        edit: "Name: ",
+        edit: "Name",
         valid: true,
         required: true,
+        minLength: 4,
       },
       {
         name: "address",
@@ -54,27 +55,64 @@ function App() {
         value: "",
         inputValue: "",
         id: uniqid(),
+        edit: "Degree",
+        valid: true,
+        required: true,
       },
       {
         name: "school",
         value: "",
         inputValue: "",
         id: uniqid(),
+        edit: "School",
+        valid: true,
+        required: true,
       },
       {
         name: "minors",
         value: "",
         inputValue: "",
         id: uniqid(),
+        edit: "Minors",
+        valid: true,
       },
       {
         name: "gradDate",
         value: "",
         inputValue: "",
         id: uniqid(),
+        edit: "Graduation Date",
+        valid: true,
+        required: true,
       },
     ],
   });
+
+  function updateInputValue(id, newValue) {
+    loopThroughInputs(
+      (input, newVal) => {
+        input.inputValue = newVal;
+      },
+      newValue,
+      id
+    );
+  }
+
+  function toggleValidity(id, newValue) {
+    loopThroughInputs(
+      (input, newVal) => {
+        input.valid = newVal;
+      },
+      newValue,
+      id
+    );
+  }
+
+  function checkValidity(inputGroup) {
+    return !inputGroup.some((input) => {
+      return input.valid === false;
+    });
+  }
 
   function loopThroughInputs(func, value = null, id = null) {
     let inputGroups = { ...inputs };
@@ -93,8 +131,30 @@ function App() {
 
   return (
     <div className="App">
-      <Info inputs={inputs.generalInputs} loop={loopThroughInputs} />
-      <Info inputs={inputs.educationInputs} loop={loopThroughInputs} />
+      <Info
+        inputs={inputs.generalInputs}
+        loop={loopThroughInputs}
+        class="generalInfo"
+        allInputsValid={checkValidity(inputs.generalInputs)}
+        update={updateInputValue}
+        toggle={toggleValidity}
+      />
+      <div className="workInfoWrapper">
+        {/**button that lets you add new Infos with startdate, enddate,
+         * title and company inputs. startdate and enddate could be linked
+         * for textfield validation.*/}
+      </div>
+      <div className="educationInfoWrapper">
+        <h2>Education</h2>
+        <Info
+          inputs={inputs.educationInputs}
+          loop={loopThroughInputs}
+          class="educationInfo"
+          allInputsValid={checkValidity(inputs.educationInputs)}
+          update={updateInputValue}
+          toggle={toggleValidity}
+        />
+      </div>
     </div>
   );
 }
