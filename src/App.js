e95bb16f340371d2c1db340a1b0e88f9151e4c94
oliverowen2210 <oliverowen2210@gmail.com>
2022-11-01 11:21:13 -0600

    added info object array, for mapping and iterating

--- src/App.js
+++ src/App.js
@@ -6,13 +6,51 @@ import NewInfoButton from "./components/NewInfoButton";
 
 function App() {
   let [educationInfos, setEducationInfos] = useState([
-    <Info
-      inputs={getInputs("education")}
-      class="educationInfo"
-      key={uniqid()}
-    />,
+    {
+      inputs: getInputs("education"),
+      class: "educationInfo",
+      key: uniqid(),
+      deletable: true,
+    },
   ]);
-  let [workInfos, setWorkInfos] = useState([]);
+  let [workInfos, setWorkInfos] = useState([
+    {
+      inputs: getInputs("work"),
+      class: "workInfo",
+      key: uniqid(),
+      deletable: true,
+    },
+  ]);
+
+  let infos = [educationInfos, workInfos];
+
+  function mapInfos(infoGroup) {
+    return infoGroup.map((info) => {
+      return (
+        <Info
+          inputs={info.inputs}
+          class={info.class}
+          key={info.key}
+          deleteFunc={() => {
+            deleteInfo(info.key);
+          }}
+          deletable={info.deletable}
+        />
+      );
+    });
+  }
+
+  function deleteInfo(key) {
+    for (let infoGroup of infos) {
+      for (let info of infoGroup) {
+        if (info.key === key) {
+          infoGroup.splice(infoGroup.indexOf(info), 1);
+          setEducationInfos([...educationInfos]);
+        }
+      }
+    }
+  }
+
   function getInputs(type) {
     if (type === "general") {
       return [
@@ -87,29 +125,81 @@ function App() {
           required: true,
         },
       ];
-    }
+    } else if (type === "work")
+      return [
+        {
+          name: "workTitle",
+          value: "",
+          inputValue: "",
+          id: uniqid(),
+          edit: "Position Title",
+          valid: true,
+          required: true,
+        },
+        {
+          name: "companyName",
+          value: "",
+          inputValue: "",
+          id: uniqid(),
+          edit: "Company",
+          valid: true,
+          required: true,
+        },
+        {
+          name: "workDates",
+          value: "",
+          inputValue: "",
+          id: uniqid(),
+          edit: "Employment Dates",
+          valid: true,
+          required: true,
+        },
+        {
+          name: "jobDesc",
+          value: "",
+          inputValue: "",
+          id: uniqid(),
+          edit: "Job description",
+          valid: true,
+          required: false,
+          textarea: true,
+        },
+      ];
   }
 
   return (
     <div className="App">
       <Info inputs={getInputs("general")} class="generalInfo" key={uniqid()} />
-      <div className="workInfoWrapper">
-        {/**button that lets you add new Infos with startdate, enddate,
-         * title and company inputs. startdate and enddate could be linked
-         * for textfield validation.*/}
-      </div>
-      <div className="educationInfoWrapper">
-        <h2>Education</h2>
-        <div className="infoGroup">{educationInfos}</div>
-        <NewInfoButton
-          infoArray={educationInfos}
-          setFunc={setEducationInfos}
-          infoInputs={getInputs("education")}
-          infoClass="educationInfo"
-          className="newEducationInfoButton"
-          limit={2}
-        />
-      </div>
+      <main>
+        <div className="infoWrapper">
+          <h2>Experience</h2>
+          <NewInfoButton
+            infoArray={workInfos}
+            setFunc={setWorkInfos}
+            infoInputs={getInputs("work")}
+            infoClass="workInfo"
+            className="newInfoButton"
+            infoDeleteFunc={deleteInfo}
+            infoDeletable={true}
+            limit={3}
+          />
+          <div className="infoGroup">{mapInfos(workInfos)}</div>
+        </div>
+        <div className="infoWrapper">
+          <h2>Education</h2>
+          <NewInfoButton
+            infoArray={educationInfos}
+            setFunc={setEducationInfos}
+            infoInputs={getInputs("education")}
+            infoClass="educationInfo"
+            className="newInfoButton"
+            infoDeleteFunc={deleteInfo}
+            infoDeletable={true}
+            limit={2}
+          />
+          <div className="infoGroup">{mapInfos(educationInfos)}</div>
+        </div>
+      </main>
     </div>
   );
 }
